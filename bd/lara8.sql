37287d96941eefd8408e2c06f273a036a04aeaf8
arman77262 <armanhaussain@gmail.com>
2021-01-23 22:43:02 +0600

    Add Database

---
+++ bd/lara8.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 23, 2021 at 05:39 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lara8`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+-- Error reading structure for table lara8.brands: #1932 - Table 'lara8.brands' doesn't exist in engine
+-- Error reading data for table lara8.brands: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lara8`.`brands`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+-- Error reading structure for table lara8.categories: #1932 - Table 'lara8.categories' doesn't exist in engine
+-- Error reading data for table lara8.categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lara8`.`categories`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+-- Error reading structure for table lara8.contacts: #1932 - Table 'lara8.contacts' doesn't exist in engine
+-- Error reading data for table lara8.contacts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lara8`.`contacts`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_forms`
+--
+-- Error reading structure for table lara8.contact_forms: #1932 - Table 'lara8.contact_forms' doesn't exist in engine
+-- Error reading data for table lara8.contact_forms: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lara8`.`contact_forms`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+-- Error reading structure for table lara8.failed_jobs: #1932 - Table 'lara8.failed_jobs' doesn't exist in engine
+-- Error reading data for table lara8.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lara8`.`failed_jobs`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `home_abouts`
+--
+-- Error reading structure for table lara8.home_abouts: #1932 - Table 'lara8.home_abouts' doesn't exist in engine
+-- Error reading data for table lara8.home_abouts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lara8`.`home_abouts`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table lara8.migrations: #1932 - Table 'lara8.migrations' doesn't exist in engine
+-- Error reading data for table lara8.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lara8`.`migrations`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `multipics`
+--
+-- Error reading structure for table lara8.multipics: #1932 - Table 'lara8.multipics' doesn't exist in engine
+-- Error reading data for table lara8.multipics: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lara8`.`multipics`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+-- Error reading structure for table lara8.password_resets: #1932 - Table 'lara8.password_resets' doesn't exist in engine
+-- Error reading data for table lara8.password_resets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lara8`.`password_resets`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+-- Error reading structure for table lara8.personal_access_tokens: #1932 - Table 'lara8.personal_access_tokens' doesn't exist in engine
+-- Error reading data for table lara8.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lara8`.`personal_access_tokens`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services`
+--
+-- Error reading structure for table lara8.services: #1932 - Table 'lara8.services' doesn't exist in engine
+-- Error reading data for table lara8.services: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lara8`.`services`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+-- Error reading structure for table lara8.sessions: #1932 - Table 'lara8.sessions' doesn't exist in engine
+-- Error reading data for table lara8.sessions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lara8`.`sessions`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sliders`
+--
+-- Error reading structure for table lara8.sliders: #1932 - Table 'lara8.sliders' doesn't exist in engine
+-- Error reading data for table lara8.sliders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lara8`.`sliders`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table lara8.users: #1932 - Table 'lara8.users' doesn't exist in engine
+-- Error reading data for table lara8.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `lara8`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
